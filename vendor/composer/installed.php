4c52e7f7201f6d0689c736c478ea71873ad3d658
LE NIKI <141644066+PrinceNiki14@users.noreply.github.com>
2025-04-07 11:12:40 +0000

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpxmlrpc/phpxmlrpc' => array(
+            'pretty_version' => '4.11.1',
+            'version' => '4.11.1.0',
+            'reference' => '06b9d7275d637f6859527091a54a5edfe8a16749',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpxmlrpc/phpxmlrpc',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
